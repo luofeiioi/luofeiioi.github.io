735652b2f028c9b3d31fa9c4c808b570939d4997
luofeiioi <30742497+luofeiioi@users.noreply.github.com>
2018-04-19 15:02:18 +0800

    ADD

--- paper-full.js
+++ paper-full.js
@@ -1,34 +1,3 @@
-/*!
- * Paper.js v0.9.25 - The Swiss Army Knife of Vector Graphics Scripting.
- * http://paperjs.org/
- *
- * Copyright (c) 2011 - 2014, Juerg Lehni & Jonathan Puckey
- * http://scratchdisk.com/ & http://jonathanpuckey.com/
- *
- * Distributed under the MIT license. See LICENSE file for details.
- *
- * All rights reserved.
- *
- * Date: Sun Oct 25 11:23:38 2015 +0100
- *
- ***
- *
- * Straps.js - Class inheritance library with support for bean-style accessors
- *
- * Copyright (c) 2006 - 2013 Juerg Lehni
- * http://scratchdisk.com/
- *
- * Distributed under the MIT license.
- *
- ***
- *
- * Acorn.js
- * http://marijnhaverbeke.nl/acorn/
- *
- * Acorn is a tiny, fast JavaScript parser written in JavaScript,
- * created by Marijn Haverbeke and released under an MIT license.
- *
- */
 
 var paper = new function(undefined) {
 
